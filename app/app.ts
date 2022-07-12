6e5b83ee92695fd616d8aaa6ce6859139f6d7bec
sojebsikder <sojebsikder@gmail.com>
2022-07-12 15:19:51 +0600

    main

--- app/app.ts
+++ app/app.ts
@@ -8,7 +8,7 @@ import { rootResolver } from "../graphql/resolvers";
 import { schema } from "../graphql/schema";
 import bodyParser from "body-parser";
 import { appConfig } from "../config/app";
-import { WebSocket } from "../system";
+import { ArrayHelper, WebSocket } from "../system";
 
 /**
  * Use any middleware here
@@ -18,26 +18,60 @@ export function boot(app: Express) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser(appConfig.cookieSecret));
   // custom middleware here
-  //
+
   // socket
   let users = [];
   WebSocket.io().on("connection", function (socket) {
+    let currentUser;
     console.log("A user connected");
-    socket.on("setUsername", function (data) {
+    // set username
+    // socket.on("setUsername", function (data) {
+    //   console.log(data);
+    //   if (users.indexOf(data) > -1) {
+    //     socket.emit(
+    //       "userExists",
+    //       data + " username is taken! Try some other username."
+    //     );
+    //   } else {
+    //     users.push(data);
+    //     socket.emit("userSet", { username: data });
+    //   }
+    // });
+    // set user id
+    socket.on("setUserId", function (data) {
       console.log(data);
+
+      let participant;
+      let randParticipant = ArrayHelper.randomElement(users);
+      if (data == randParticipant) {
+        randParticipant = ArrayHelper.randomElement(users);
+      } else {
+        participant = randParticipant;
+      }
+
+      console.log(users);
+      currentUser = data;
+
       if (users.indexOf(data) > -1) {
-        socket.emit(
-          "userExists",
-          data + " username is taken! Try some other username."
-        );
+        // if exists userid then emit userExists
+        socket.emit("userExists", data + " Please start again.");
       } else {
         users.push(data);
-        socket.emit("userSet", { username: data });
+        socket.emit("userSet", { username: data, participant: participant });
       }
     });
+
     socket.on("msg", function (data) {
       //Send message to everyone
       WebSocket.io().sockets.emit("newmsg", data);
+    });
+
+    socket.on("disconnect", function () {
+      users = users.filter(function (v) {
+        return v != currentUser;
+      });
+
+      console.log("A user disconnected", currentUser);
     });
   });
 
