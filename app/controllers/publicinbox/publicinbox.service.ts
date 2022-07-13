5633f3d0a0985a4608a304a0cfc76753a757ecca
sojebsikder <sojebsikder@gmail.com>
2022-07-13 12:23:57 +0600

    main

--- app/controllers/publicinbox/publicinbox.service.ts
+++ app/controllers/publicinbox/publicinbox.service.ts
@@ -18,46 +18,19 @@ export class PublicinboxService {
   /**
    * show all data
    */
-  public async index() {
-    const result = await prisma.post.findMany();
-    return result;
-  }
+  public async index() {}
 
   /**
    * show specific data
    * @param req
    * @param res
    */
-  async show(arg_id: string) {
-    const id = arg_id;
-    const result = await prisma.post.findFirst({
-      where: {
-        id: Number(id),
-      },
-    });
-    return result;
-  }
+  async show(arg_id: string) {}
 
   /**
    * store data
    * @param req
    * @param res
    */
-  async store(req: Request, res: Response) {
-    const title = req.body.title;
-    const content = req.body.content;
-
-    const user = Auth.userByCookie(req.signedCookies);
-
-    const post = {
-      title: title,
-      content: content,
-      authorId: user.userid,
-    };
-
-    const result = await prisma.post.create({
-      data: post,
-    });
-  }
+  async store(req: Request, res: Response) {}
 }
- 
