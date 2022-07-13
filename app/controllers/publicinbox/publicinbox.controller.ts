a869aa3673db2087e28eb87138a4e830d5e63557
sojebsikder <sojebsikder@gmail.com>
2022-07-13 12:52:29 +0600

    main

--- app/controllers/publicinbox/publicinbox.controller.ts
+++ app/controllers/publicinbox/publicinbox.controller.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from "express";
 import { Controller, Get } from "../../../system/decorator";
+import { decorateHtmlResponse } from "../../middlewares/common/decorateHtmlResponse";
 
 @Controller("/public/")
 export class PublicinboxController {
   //
-  @Get("")
+  @Get("", { middleware: [decorateHtmlResponse()] })
   async index(req: Request, res: Response) {
     res.render("publicinbox");
-  }
-  @Get("/about")
-  async about(req: Request, res: Response) {
-    res.send("Hello world");
   }
 }
